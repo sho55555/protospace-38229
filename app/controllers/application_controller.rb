977e4165d1572f305365341ebf0fffb857e57d53
sho55555 <shogo61019@gmail.com>
2022-07-29 14:48:45 +0900

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configue_permitted_parameters, if: :devise_controller?
+
+  private
+  def configue_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
+  end
 end
